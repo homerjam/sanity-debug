f844f6cef2984c43ab057a9ba548940b5ee1d610
James Homer <james@homerjam.es>
2024-06-25 12:22:30 +0100

    test

--- src/lib/sanity/schema/index.tsx
+++ src/lib/sanity/schema/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {defineField} from 'sanity'
 
 export const schemaTypes: SanitySchemaTypeExtended[] = [
   {
@@ -11,6 +12,26 @@ export const schemaTypes: SanitySchemaTypeExtended[] = [
         name: 'title',
         title: 'Title',
       },
+      defineField({
+        name: 'computed',
+        title: 'Computed',
+        // description: '...',
+        type: 'computedNumber',
+        readOnly: true,
+        options: {
+          buttonText: 'Refresh',
+          documentQuerySelection: `
+            "numberOfReferences": count(*[references(^._id)])
+          `,
+          reduceQueryResult: (result: {
+            draft?: {numberOfReferences: number}
+            published: {numberOfReferences: number}
+          }) => {
+            // When 'Refresh' button is pressed, this value will be set to result.published.numberOfReferences, from the documentQuerySelection above.
+            return result.published.numberOfReferences
+          },
+        },
+      }),
     ],
     preview: {
       prepare: () => ({
